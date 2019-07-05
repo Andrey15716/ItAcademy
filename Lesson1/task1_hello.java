56d1547b5f19c124a5c9c2fdd27a7b23df350866
Andrey15716 <52579943+Andrey15716@users.noreply.github.com>
2019-07-05 21:01:20 +0300

    Update task1_hello.java

--- Lesson1/task1_hello.java
+++ Lesson1/task1_hello.java
@@ -3,8 +3,7 @@ package Lesson1;
 public class Task1 {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        // Prints "Hello, World" 
+        System.out.println("Hello");
     }
-
 }
